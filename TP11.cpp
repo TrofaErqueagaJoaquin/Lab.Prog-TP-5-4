539d8ec691d276637dd8ac21173c4884a351430a
Trofa01 <133694574+TrofaErqueagaJoaquin@users.noreply.github.com>
2024-04-19 11:30:21 -0300

    TP11.cpp

--- TP11.cpp
+++ TP11.cpp
@@ -6,28 +6,29 @@ int main() {
     cin >> n >> m;
 
     vector<vector<char>> matrix(n, vector<char>(m));
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             if (i % 2 == 0) {
                 matrix[i][j] = 'L';
-            } else {
+            }
+            if (i % 2 != 0) {
                 matrix[i][j] = 'O';
             }
         }
     }
 
     int q = 0;
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < m - 1; ++j) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < m - 1; j++) {
             if (matrix[i][j] == 'L' && matrix[i][j + 1] == 'O' && matrix[i + 1][j] == 'L') {
-                ++q;
+                q++;
             }
         }
     }
 
     cout << n << " " << m << " " << q << "\n";
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
+        for (int j = 0; j < m; j++) {
             cout << matrix[i][j];
         }
         cout << "\n";
